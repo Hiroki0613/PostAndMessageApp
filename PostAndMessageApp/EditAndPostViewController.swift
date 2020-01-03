aff7f0918508fe5239690606d2cf22cc9fe16748
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-03 21:37:18 +0900

    EditAndPostViewControllerのUI実装

---
+++ PostAndMessageApp/EditAndPostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditAndPostViewController.swift
+//  PostAndMessageApp
+//
+//  Created by 宏輝 on 03/01/2020.
+//  Copyright © 2020 宏輝. All rights reserved.
+//
+
+import UIKit
+
+class EditAndPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
