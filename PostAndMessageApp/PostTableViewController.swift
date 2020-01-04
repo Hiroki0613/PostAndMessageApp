5195d9ce22769e1fc3a73302587c4b0cbcb2c7f7
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-04 22:29:52 +0900

    Assetsに画像を追加

---
+++ PostAndMessageApp/PostTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PostTableViewController.swift
+//  PostAndMessageApp
+//
+//  Created by 宏輝 on 04/01/2020.
+//  Copyright © 2020 宏輝. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewController: UIViewController {
+    
+    @IBOutlet weak var timeLineTableView: UITableView!
+    
+
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
